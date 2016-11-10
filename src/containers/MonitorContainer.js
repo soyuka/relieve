2aea42589f7b1f63b509e05843b6ec288604c8c8
abluchet <abluchet@ds-restauration.com>
2016-11-10 10:45:52 +0100

    feat(): Add cpu percentage to monitor

--- src/containers/MonitorContainer.js
+++ src/containers/MonitorContainer.js
@@ -2,15 +2,21 @@ if (!process.relieve) {
   require('./CallableContainer.js')
 }
 
+const os = require('os')
 const ipc = process.relieve.ipc
 
 let cpuUsage
+let time
 
 ipc.on('usage', function() {
   cpuUsage = process.cpuUsage(cpuUsage)
+  time = process.hrtime(time)
+
+  let cpuPercent = (100 * (cpuUsage.system / 1000 + cpuUsage.user / 1000) / (time[0] * 1000 + time[1] / 1e6))
 
   ipc.send('usage', {
     cpu: cpuUsage,
+    cpuPercent: cpuPercent,
     memory: process.memoryUsage()
   })
 })
