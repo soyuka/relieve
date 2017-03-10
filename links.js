0f62b5010296a7a22a05395185348acffbaf6aab
abluchet <abluchet@ds-restauration.com>
2017-03-10 12:37:43 +0100

    Improve postinstall links remove/link again
    When the absolute path of the package changes, links will be broken

--- links.js
+++ links.js
@@ -6,7 +6,11 @@ directories.forEach(function(d) {
   var path = p.resolve(__dirname, 'src', d)
   var dest = p.resolve(__dirname, d)
 
-  try {
+  fs.access(dest, fs.constants.F_OK | fs.constants.R_OK, function(err) {
+    if (!err) {
+      fs.unlinkSync(dest)
+    }
+
     fs.symlinkSync(path, dest, 'dir')
-  } catch(e) {}
+  })
 })
